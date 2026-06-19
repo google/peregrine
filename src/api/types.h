17764e8f8611e7331fa9dadb863a7aa1ba906318
Yong Xia <yongx@google.com>
2026-06-19 02:48:15 +0000

    [peregrine] update `tpu_raiden` dependency
    PiperOrigin-RevId: 934679749

--- src/api/types.h
+++
@@ -1,7 +0,0 @@
-#ifndef PEREGRINE_SRC_API_TYPES_H_
-#define PEREGRINE_SRC_API_TYPES_H_
-
-// TODO(yongx): Remove this file once all dependencies are updated.
-#include "src/api/transport_types.h"
-
-#endif  // PEREGRINE_SRC_API_TYPES_H_
