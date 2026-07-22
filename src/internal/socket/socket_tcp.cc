e46148a65281681e106ca28cd2bf736cbf330b17
Yong Xia <yongx@google.com>
2026-07-22 16:44:14 +0000

    [peregrine] add vectorized socket send/recv functions
    PiperOrigin-RevId: 952175100

--- src/internal/socket/socket_tcp.cc
+++ src/internal/socket/socket_tcp.cc
@@ -12,6 +12,7 @@
 #include <memory>
 #include <string>
 #include <string_view>
+#include <vector>
 
 #include "absl/base/optimization.h"
 #include "absl/log/check.h"
@@ -20,11 +21,13 @@
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
+#include "absl/types/span.h"
 #include "src/api/transport_types.h"
 #include "src/internal/base/endpoint.h"
 #include "src/internal/base/types.h"
 #include "src/internal/socket/socket_base.h"
 #include "src/internal/socket/socket_util.h"
+#include "src/internal/util/util.h"
 
 namespace peregrine::internal {
 
@@ -192,7 +195,7 @@ ssize_t TcpSocket::Recv(Byte* const buf, const size_t len) const {
       DCHECK_EQ(buf + len, ptr + left);
       VLOG(1) << ioMsg("recv", bytes);
     } else if (bytes == 0) {  // peer closed connection
-      LOG(INFO) << ioMsg("recv EoF", 0);
+      LOG(INFO) << ioMsg("recv eof", 0);
       return 0;
     } else {
       const auto last_errno = errno;
@@ -217,10 +220,11 @@ inline std::string ErrMsg(std::string_view what, fd_t fd, int last_errno) {
 
 /*static*/ absl::Status TcpSocket::Send(const fd_t fd, const Byte* const buf,
                                         const size_t len) {
-  DCHECK_GE(len, 1);
-  DCHECK_LE(len, std::numeric_limits<ssize_t>::max());
   DCHECK(IsValidSocket(fd));
   DCHECK(IsBlockingMode(fd));
+
+  DCHECK_LE(len, std::numeric_limits<ssize_t>::max());
+  if (len == 0) return absl::OkStatus();
 
   const Byte* ptr = buf;
   size_t sent = 0;
@@ -252,10 +256,11 @@ inline std::string ErrMsg(std::string_view what, fd_t fd, int last_errno) {
 
 /*static*/ absl::Status TcpSocket::Recv(const fd_t fd, Byte* const buf,
                                         const size_t len) {
-  DCHECK_GE(len, 1);
-  DCHECK_LE(len, std::numeric_limits<ssize_t>::max());
   DCHECK(IsValidSocket(fd));
   DCHECK(IsBlockingMode(fd));
+
+  DCHECK_LE(len, std::numeric_limits<ssize_t>::max());
+  if (len == 0) return absl::OkStatus();
 
   Byte* ptr = buf;
   size_t rcvd = 0;
@@ -269,7 +274,7 @@ inline std::string ErrMsg(std::string_view what, fd_t fd, int last_errno) {
       rcvd += bytes;
       DCHECK_EQ(buf + len, ptr + left);
     } else if (bytes == 0) {  // peer closed connection
-      return absl::InternalError("recv EoF");
+      return absl::InternalError("recv eof");
     } else {
       const auto last_errno = errno;
       if (Interrupted(last_errno)) continue;
@@ -278,6 +283,93 @@ inline std::string ErrMsg(std::string_view what, fd_t fd, int last_errno) {
     }
   }
   DCHECK_EQ(left, 0);
+  DCHECK_EQ(rcvd, len);
+  return absl::OkStatus();
+}
+
+/*static*/ absl::Status TcpSocket::SendV(const fd_t fd,
+                                         const absl::Span<const IoVec> iovecs) {
+  DCHECK(IsValidSocket(fd));
+  DCHECK(IsBlockingMode(fd));
+  DCHECK_LE(iovecs.size(), IOV_MAX);
+
+  const size_t len = TotalLength(iovecs);
+  DCHECK_LE(len, std::numeric_limits<ssize_t>::max());
+  if (len == 0) return absl::OkStatus();
+
+  std::vector<struct iovec> vecs{iovecs.begin(), iovecs.end()};
+  const int n = vecs.size();
+  size_t sent = 0;
+  int i = 0;
+  while (i < n) {
+    const ssize_t bytes = ::writev(fd.value(), &vecs[i], n - i);
+    if ABSL_PREDICT_TRUE (bytes > 0) {
+      sent += bytes;
+      size_t b = static_cast<size_t>(bytes);
+      if ABSL_PREDICT_TRUE (b >= len) break;
+      while (i < n && vecs[i].iov_len <= b) {  // advance iov index
+        b -= vecs[i].iov_len;
+        ++i;
+      }
+      if (i >= n) break;
+      if (b > 0) {  // adjust iov ptr/len
+        vecs[i].iov_base = static_cast<Byte*>(vecs[i].iov_base) + b;
+        vecs[i].iov_len -= b;
+      }
+    } else {
+      if ABSL_PREDICT_TRUE (bytes < 0) {
+        const auto last_errno = errno;
+        if (Interrupted(last_errno)) continue;
+        DCHECK(!WouldBlock(last_errno));
+        return absl::InternalError(ErrMsg("writev", fd, last_errno));
+      } else {  // rarely happens
+        DCHECK_EQ(bytes, 0);
+        return absl::InternalError("writev zero");
+      }
+    }
+  }
+  DCHECK_EQ(sent, len);
+  return absl::OkStatus();
+}
+
+/*static*/ absl::Status TcpSocket::RecvV(const fd_t fd,
+                                         const absl::Span<const IoVec> iovecs) {
+  DCHECK(IsValidSocket(fd));
+  DCHECK(IsBlockingMode(fd));
+  DCHECK_LE(iovecs.size(), IOV_MAX);
+
+  const size_t len = TotalLength(iovecs);
+  DCHECK_LE(len, std::numeric_limits<ssize_t>::max());
+  if (len == 0) return absl::OkStatus();
+
+  std::vector<struct iovec> vecs{iovecs.begin(), iovecs.end()};
+  const int n = vecs.size();
+  size_t rcvd = 0;
+  int i = 0;
+  while (i < n) {
+    const ssize_t bytes = ::readv(fd.value(), &vecs[i], n - i);
+    if ABSL_PREDICT_TRUE (bytes > 0) {
+      rcvd += bytes;
+      size_t b = static_cast<size_t>(bytes);
+      if ABSL_PREDICT_TRUE (b >= len) break;
+      while (i < n && vecs[i].iov_len <= b) {  // advance iov index
+        b -= vecs[i].iov_len;
+        ++i;
+      }
+      if (i >= n) break;
+      if (b > 0) {  // adjust iov ptr/len
+        vecs[i].iov_base = static_cast<Byte*>(vecs[i].iov_base) + b;
+        vecs[i].iov_len -= b;
+      }
+    } else if (bytes == 0) {  // peer closed connection
+      return absl::InternalError("readv eof");
+    } else {
+      const auto last_errno = errno;
+      if (Interrupted(last_errno)) continue;
+      DCHECK(!WouldBlock(last_errno));
+      return absl::InternalError(ErrMsg("readv", fd, last_errno));
+    }
+  }
   DCHECK_EQ(rcvd, len);
   return absl::OkStatus();
 }
