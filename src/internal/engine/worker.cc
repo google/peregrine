d6908171e8ea68a970ece5f17929f4ccf8d47a74
Swasthi Shetty <swasthi@google.com>
2026-09-18 18:07:07 -0700

    [peregrine] Add on_complete callback to Post API (4/N)
    PiperOrigin-RevId: 984166054

--- src/internal/engine/worker.cc
+++ src/internal/engine/worker.cc
@@ -106,8 +106,7 @@ void Worker::SendLoop() {
       // ACK chunk sent by the receiver.
       static_assert(
           assumptions::kOneSidedRdmaCompletionIsTrackedBySenderHardwareCqe);
-      outgoing_.FindOrCreate(chunk.handle, chunk.reqid, chunk.nchunks)
-          .Set(chunk.index);
+      outgoing_.Set(chunk.handle, chunk.reqid, chunk.nchunks, chunk.index);
     }
   }
 }
